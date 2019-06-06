774ef88415fb197b248c17a7fcf51b1dd407f16f
Juix <5263190+Juiix@users.noreply.github.com>
2019-06-06 13:18:02 -0600

    Udp work

--- Utils.NET/Net/Udp/UdpClient.cs
+++ Utils.NET/Net/Udp/UdpClient.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using Utils.NET.IO;
+using Utils.NET.Logging;
 
 namespace Utils.NET.Net.Udp
 {
@@ -43,11 +46,32 @@ namespace Utils.NET.Net.Udp
         /// </summary>
         private IO.Buffer buffer;
 
+        /// <summary>
+        /// Object used to sync sending states
+        /// </summary>
+        private object sendSync = new object();
+
+        /// <summary>
+        /// Bool determining if the client is currently sending
+        /// </summary>
+        private bool sending = false;
+
+        /// <summary>
+        /// Queue used to store data ready to send
+        /// </summary>
+        private Queue<TPacket> sendQueue = new Queue<TPacket>();
+
         #region Init
 
         public UdpClient()
         {
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Init();
+        }
+
+        public UdpClient(Socket socket)
+        {
+            this.socket = socket;
             Init();
         }
 
@@ -111,15 +135,94 @@ namespace Utils.NET.Net.Udp
         private void OnRead(IAsyncResult ar)
         {
             int length = socket.EndReceiveFrom(ar, ref remoteEndPoint);
+
+            byte[] data = buffer.data;
+            BitReader r = new BitReader(data, length);
+            byte id = r.ReadUInt8();
+            TPacket packet = packetFactory.CreatePacket(id);
+            if (packet == null)
+            {
+                Log.Error($"No {typeof(TPacket).Name} for id: {id}");
+                return;
+            }
+            packet.ReadPacket(r);
+            HandlePacket(packet);
+        }
+
+        /// <summary>
+        /// Handles a received packet
+        /// </summary>
+        /// <param name="packet">Packet.</param>
+        private void HandlePacket(TPacket packet)
+        {
+
         }
 
         #endregion
 
         #region Sending
 
-        private void Send()
+        /// <summary>
+        /// Sends a given packet
+        /// </summary>
+        /// <param name="packet">Packet.</param>
+        public void Send(TPacket packet)
         {
+            lock (sendSync)
+            {
+                if (sending)
+                {
+                    sendQueue.Enqueue(packet);
+                    return;
+                }
+                sending = true;
+            }
 
+            SendPacket(packet);
+        }
+
+        /// <summary>
+        /// Sends a packet to the remote 
+        /// </summary>
+        /// <param name="packet">Packet.</param>
+        private void SendPacket(TPacket packet)
+        {
+            var package = PackagePacket(packet);
+            socket.BeginSendTo(package.data, 0, package.size, SocketFlags.None, remoteEndPoint, OnSend, null);
+        }
+
+        /// <summary>
+        /// Callback function for socket send calls
+        /// </summary>
+        /// <param name="ar">Ar.</param>
+        private void OnSend(IAsyncResult ar)
+        {
+            int sent = socket.EndSendTo(ar);
+
+            TPacket nextPacket;
+            lock (sendSync)
+            {
+                if (sendQueue.Count == 0)
+                {
+                    sending = false;
+                    return;
+                }
+                nextPacket = sendQueue.Dequeue();
+            }
+
+            SendPacket(nextPacket);
+        }
+
+        /// <summary>
+        /// Packages a packet into a buffer to send
+        /// </summary>
+        /// <returns>The packet.</returns>
+        /// <param name="packet">Packet.</param>
+        private IO.Buffer PackagePacket(TPacket packet)
+        {
+            var w = new BitWriter();
+            packet.WritePacket(w);
+            return w.GetData();
         }
 
         #endregion
