0dbaf33f346659115cac86819c614e17c40e75f1
Tucker <5263190+Juiix@users.noreply.github.com>
2019-06-06 15:15:35 -0600

    Adding Udp packets

---
+++ Utils.NET/Net/Udp/Packets/UdpDisconnect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils.NET.IO;
+
+namespace Utils.NET.Net.Udp.Packets
+{
+    public class UdpDisconnect : UdpPacket
+    {
+        public override UdpPacketType Type => UdpPacketType.Disconnect;
+
+        /// <summary>
+        /// Generated salt from server/client
+        /// </summary>
+        public ulong salt;
+
+        public UdpDisconnect(ulong salt) => this.salt = salt;
+
+        protected override void Read(BitReader r)
+        {
+            salt = r.ReadUInt64();
+        }
+
+        protected override void Write(BitWriter w)
+        {
+            w.Write(salt);
+        }
+    }
+}
---
+++ Utils.NET/Net/Udp/Packets/UdpPacketType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.NET.Net.Udp.Packets
+{
+    public enum UdpPacketType : byte
+    {
+        Connect,
+        Challenge,
+        Solution,
+        Disconnect
+    }
+}
