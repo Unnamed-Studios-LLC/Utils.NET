f159629660d26c7087a6f267e07d588e563d6e07
Tucker <5263190+Juiix@users.noreply.github.com>
2019-06-07 17:00:01 -0600

    Udp listener work

--- Utils.NET/Net/Udp/UdpListener.cs
+++ Utils.NET/Net/Udp/UdpListener.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Utils.NET.Net.Udp
@@ -11,18 +12,63 @@ namespace Utils.NET.Net.Udp
         where TCon : UdpClient<TPacket>
     {
         /// <summary>
-        /// The max amount of clients that can connect
-        /// </summary>
-        private int maxClients;
-
-        /// <summary>
         /// The port that the listener listens on
         /// </summary>
-        private ushort port;
+        private int port;
+
+        /// <summary>
+        /// A queue consisting of all available ports
+        /// </summary>
+        private ConcurrentQueue<int> availablePorts = new ConcurrentQueue<int>();
+
+        /// <summary>
+        /// Socket used to receive connection packets
+        /// </summary>
+        private Socket socket;
+
+        #region Init
 
         public UdpListener(ushort port, int maxClients)
         {
+            this.port = port;
+            InitPorts(maxClients);
+            InitSocket();
+        }
+
+        /// <summary>
+        /// Fills the available ports queue will all ports
+        /// </summary>
+        /// <param name="maxClients"></param>
+        private void InitPorts(int maxClients)
+        {
+            for (int i = 1; i <= maxClients; i++)
+            {
+                availablePorts.Enqueue(port + i);
+            }
+        }
+
+        /// <summary>
+        /// Initializes the socket for catching incoming connections
+        /// </summary>
+        private void InitSocket()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, port));
+        }
+
+        #endregion
+
+        #region Reading
+
+        /// <summary>
+        /// Starts accepting new clients
+        /// </summary>
+        public void Start()
+        {
 
         }
+
+        #endregion
+
     }
 }
