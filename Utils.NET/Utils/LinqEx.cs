99ecb8e71c217715789dd52ebde0060d5033f182
Tucker <5263190+Juiix@users.noreply.github.com>
2019-12-12 02:25:23 -0700

    Linq extensions

---
+++ Utils.NET/Utils/LinqEx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.NET.Utils
+{
+    public static class LinqEx
+    {
+        public static T Min<T>(this IEnumerable<T> collection, Func<T, T, T> comparison)
+        {
+            T result = default(T);
+            foreach (var value in collection)
+            {
+                if (result == null)
+                {
+                    result = value;
+                    continue;
+                }
+
+                result = comparison(result, value);
+            }
+            return result;
+        }
+
+        public static T Closest<T>(this IEnumerable<T> collection, Func<T, float> distance)
+        {
+            T result = default(T);
+            float closestDistance = float.MaxValue;
+            foreach (var value in collection)
+            {
+                var dis = distance(value);
+                if (dis < closestDistance)
+                {
+                    result = value;
+                    closestDistance = dis;
+                }
+            }
+            return result;
+        }
+    }
+}
