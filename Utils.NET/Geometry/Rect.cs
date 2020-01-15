874343a99356e71022e411bee122a4223f66b35a
Tucker <5263190+Juiix@users.noreply.github.com>
2020-01-14 19:52:10 -0700

    Added GetIntRect()

--- Utils.NET/Geometry/Rect.cs
+++ Utils.NET/Geometry/Rect.cs
@@ -66,9 +66,19 @@ namespace Utils.NET.Geometry
             height = size.y;
         }
 
+        /*
         public IntRect GetBoundingRect()
         {
             return new IntRect((int)Math.Floor(x), (int)Math.Floor(y), (int)Math.Ceiling(x + width), (int)Math.Ceiling(y + height));
+        }
+        */
+
+        public IntRect GetIntRect()
+        {
+            Int2 bl = BottomLeft;
+            Int2 tr = TopRight;
+
+            return new IntRect(bl.x, bl.y, tr.x - bl.x, tr.y - bl.y);
         }
 
         public override string ToString() => $"{{ {x}, {y}, {width}, {height} }}";
