483e968b206f9e66eac5f072ad5691320f52b3bb
Tucker <5263190+Juiix@users.noreply.github.com>
2020-05-22 12:46:17 -0600

    Template string class
    Used to create a templated string with preset key/value replacements

---
+++ Utils.NET/Utils/TemplateString.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils.NET.Logging;
+
+namespace Utils.NET.Utils
+{
+    public class TemplateString
+    {
+        private string[] bodyStrings;
+
+        private string[] keys;
+
+        public TemplateString(string template, char keyCharacter)
+        {
+            bodyStrings = template.Split(keyCharacter);
+            keys = new string[bodyStrings.Length - 1];
+            for (int i = 1; i < bodyStrings.Length; i++)
+            {
+                bodyStrings[i] = GetKeyAndTruncate(bodyStrings[i], out var key);
+                keys[i - 1] = key;
+            }
+        }
+
+        private string GetKeyAndTruncate(string input, out string word)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+                if (!char.IsLetter(c))
+                {
+                    word = input.Substring(0, i);
+                    return input.Substring(i);
+                }
+            }
+
+            word = input;
+            return "";
+        }
+
+        public string Build(Dictionary<string, string> keyValues)
+        {
+            var builder = new StringBuilder(bodyStrings[0]);
+            for (int i = 1; i < bodyStrings.Length; i++)
+            {
+                var key = keys[i - 1];
+                if (!keyValues.TryGetValue(key, out var value))
+                    value = key;
+
+                builder.Append(value);
+                builder.Append(bodyStrings[i]);
+            }
+            return builder.ToString();
+        }
+    }
+}
