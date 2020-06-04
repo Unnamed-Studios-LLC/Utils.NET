866fca10d6e60c15240e2d06571f325ce4ee64b1
Tucker <5263190+Juiix@users.noreply.github.com>
2020-06-03 20:16:04 -0600

    Random Array Element Extension

---
+++ Utils.NET/Utils/ArrayUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.NET.Utils
+{
+    public static class ArrayUtils
+    {
+
+        /// <summary>
+        /// Returns a random array element using Rand.Next
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static T Random<T>(this T[] array)
+        {
+            if (array.Length == 0) return default;
+            return array[Rand.Next(array.Length)];
+        }
+    }
+}
