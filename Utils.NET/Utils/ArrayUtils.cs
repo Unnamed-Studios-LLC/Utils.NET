32b43a45ef935c3132fa65a851ad89966aba24a6
Tucker <5263190+Juiix@users.noreply.github.com>
2020-06-10 22:22:33 -0600

    Randomize Array

--- Utils.NET/Utils/ArrayUtils.cs
+++ Utils.NET/Utils/ArrayUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Utils.NET.Utils
@@ -17,6 +18,26 @@ namespace Utils.NET.Utils
         {
             if (array.Length == 0) return default;
             return array[Rand.Next(array.Length)];
+        }
+
+        /// <summary>
+        /// Randomizes the element order of this array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static T[] Randomize<T>(this T[] array)
+        {
+            var list = array.ToList();
+            var newArray = new T[array.Length];
+            int index = 0;
+            while (list.Count > 0)
+            {
+                var rnd = Rand.Next(list.Count);
+                newArray[index++] = list[rnd];
+                list.RemoveAt(rnd);
+            }
+            return newArray;
         }
     }
 }
