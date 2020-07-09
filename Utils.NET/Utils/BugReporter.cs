b417ad65cdfd85558764b17dbe9fcc11b302983d
Tucker <5263190+Juiix@users.noreply.github.com>
2020-07-08 19:22:42 -0600

    Bug reporter

---
+++ Utils.NET/Utils/BugReporter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.NET.Utils
+{
+    public static class BugReporter
+    {
+        static BugReporter()
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledException;
+        }
+
+        private static event Action<string> sendBugAction;
+
+        public static void Setup(Action<string> sendBugAction)
+        {
+            BugReporter.sendBugAction += sendBugAction;
+        }
+
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            sendBugAction?.Invoke(args.ExceptionObject.ToString());
+        }
+    }
+}
