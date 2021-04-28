6cda8fdbfb10f4de83c37f574b496625bebc5054
Tucker <5263190+Juiix@users.noreply.github.com>
2021-04-27 23:26:22 -0600

    Updated Rsa tools into CryptoProvider

--- Utils.NET/Crypto/CryptoProvider.cs
+++ Utils.NET/Crypto/CryptoProvider.cs
@@ -7,14 +7,51 @@ namespace Utils.NET.Crypto
 {
     public class CryptoProvider : ICryptoProvider
     {
-        private SHA1 sha1;
+        private readonly SHA1 sha1;
 
-        private SHA256 sha256;
+        private readonly SHA256 sha256;
+
+        private readonly RSA rsa;
 
         public CryptoProvider()
         {
             sha1 = SHA1.Create();
             sha256 = SHA256.Create();
+            rsa = RSA.Create();
+        }
+
+        public byte[] DecryptRsa(byte[] data)
+        {
+            return rsa.Decrypt(data, RSAEncryptionPadding.Pkcs1);
+        }
+
+        public string DecryptRsaBase64(string base64)
+        {
+            return DecryptRsaBase64(base64, Encoding.UTF8);
+        }
+
+        public string DecryptRsaBase64(string base64, Encoding encoding)
+        {
+            var encrypted = Convert.FromBase64String(base64);
+            var bytes = DecryptRsa(encrypted);
+            return encoding.GetString(bytes);
+        }
+
+        public byte[] EncryptRsa(byte[] data)
+        {
+            return rsa.Encrypt(data, RSAEncryptionPadding.Pkcs1);
+        }
+
+        public string EncryptRsaBase64(string value)
+        {
+            return EncryptRsaBase64(value, Encoding.UTF8);
+        }
+
+        public string EncryptRsaBase64(string value, Encoding encoding)
+        {
+            var bytes = encoding.GetBytes(value);
+            var encrypted = EncryptRsa(bytes);
+            return Convert.ToBase64String(encrypted);
         }
 
         public byte[] HashSha1(byte[] input)
@@ -45,6 +82,26 @@ namespace Utils.NET.Crypto
         public string HashSha256(string input)
         {
             return HashSha256(input, Encoding.UTF8);
+        }
+
+        public void SetRsaPrivateKey(RSAParameters parameters)
+        {
+            rsa.ImportParameters(parameters);
+        }
+
+        public void SetRsaPrivateKey(RsaSerializableParameters parameters)
+        {
+            rsa.ImportParameters(parameters.GetParameters());
+        }
+
+        public void SetRsaPublicKey(RSAParameters parameters)
+        {
+            rsa.ImportParameters(parameters);
+        }
+
+        public void SetRsaPublicKey(RsaSerializableParameters parameters)
+        {
+            rsa.ImportParameters(parameters.GetParameters());
         }
     }
 }
--- Utils.NET/Crypto/ICryptoProvider.cs
+++ Utils.NET/Crypto/ICryptoProvider.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Utils.NET.Crypto
 {
     public interface ICryptoProvider
     {
+        byte[] DecryptRsa(byte[] data);
+
+        string DecryptRsaBase64(string base64);
+
+        string DecryptRsaBase64(string base64, Encoding encoding);
+
+        byte[] EncryptRsa(byte[] data);
+
+        string EncryptRsaBase64(string value);
+
+        string EncryptRsaBase64(string value, Encoding encoding);
+
         byte[] HashSha1(byte[] input);
 
         string HashSha1(string input, Encoding encoding);
@@ -16,5 +29,13 @@ namespace Utils.NET.Crypto
         string HashSha256(string input, Encoding encoding);
 
         string HashSha256(string input);
+
+        void SetRsaPublicKey(RSAParameters parameters);
+
+        void SetRsaPublicKey(RsaSerializableParameters parameters);
+
+        void SetRsaPrivateKey(RSAParameters parameters);
+
+        void SetRsaPrivateKey(RsaSerializableParameters parameters);
     }
 }
